35ee17aadd0a51f6a941e5041430d5dd6359d355
Annem-Vijitha <59609333+Annem-Vijitha@users.noreply.github.com>
2023-06-06 10:47:02 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+
+import ContextConnector from "./config/connector";
+
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import Home from "./Pages/Home";
+import Items from "./Pages/Items";
+import Categories from "./Pages/Categories";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextConnector>
+      <Router>
+        <div>
+        <Navbar />
+        <Switch>
+          <Container>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/categories" component={Categories} />
+            <Route exact path="/items" component={Items} />
+          </Container>
+        </Switch>
+        </div>
+      </Router>
+    </ContextConnector>
   );
 }
 
