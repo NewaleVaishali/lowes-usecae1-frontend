62c2d6403712ffecf22ef05fc93bacf495887630
NewaleVaishali <99384467+NewaleVaishali@users.noreply.github.com>
2023-06-10 08:02:27 +0530

    AddCategoryForm.js

--- src/components/AddCategoryForm.js
+++ src/components/AddCategoryForm.js
@@ -1,48 +1,63 @@
 import React, { useContext, useState } from "react";
-
 import { Form, Modal, Button } from "semantic-ui-react";
-
 import Context from "../config/context";
 
 export default function AddCategoryForm() {
   const context = useContext(Context);
   const { addCategory } = context;
 
-  const [categoryID, setID] = useState("");
+  const [categoryID, setCategoryID] = useState(0);
   const [categoryName, setName] = useState("");
   const [categoryDescription, setDescription] = useState("");
 
-  const handleChange1 = (e, { value }) => setName({ value });
-  const handleChange2 = (e, { value }) => setDescription({ value });
+  const handleChange1 = (e) => {
+    const value = parseInt(e.target.value);
+    if (!isNaN(value)) {
+      setCategoryID(value);
+    }
+  };
+
+  const handleChange2 = (e, { value }) => setName(value);
+  const handleChange3 = (e, { value }) => setDescription(value);
 
   const handleSubmit = () => {
     const category = {
-      id: categoryID.vale,
-      name: categoryName.value,
-      description : categoryDescription.value,
+      categoryID: categoryID,
+      categoryName: categoryName,
+      categoryDescription: categoryDescription
     };
-
-    addCategory(category);
+    console.log("Submitting category:", category); // Debug statement
+    const jsonData = JSON.stringify(category);
+    console.log("Submitting category:", jsonData);
+    addCategory(jsonData);
   };
 
   return (
-    <Modal trigger={<Button fluid primary>Add new Category</Button>}>
+    <Modal trigger={<Button primary>Add new Category</Button>}>
       <Modal.Header>Add new Category</Modal.Header>
       <Modal.Content>
         <Form onSubmit={handleSubmit}>
+        <Form.Input
+        type="number"
+        name="categoryID"
+        label="Number"
+        placeholder="Category ID"
+        onChange={handleChange1}
+        value={categoryID}
+      />
           <Form.Input
             name="categoryName"
             label="Name"
             placeholder="Category name"
-            onChange={handleChange1}
-            value={categoryName.value}
+            onChange={handleChange2}
+            value={categoryName}
           />
-           <Form.Input
+          <Form.Input
             name="categoryDescription"
             label="Description"
             placeholder="Category Description"
-            onChange={handleChange2}
-            value={categoryDescription.value}
+            onChange={handleChange3}
+            value={categoryDescription}
           />
           <Button type="submit">Add</Button>
         </Form>
